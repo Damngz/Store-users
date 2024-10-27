924c101411f14736241a117c16792f7ed386e557
Damngz <da.munozgz@gmail.com>
2024-10-27 01:09:03 -0300

    base project

---
+++ src/test/java/com/example/store_users/StoreUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
