418ed15d7fefa28521f1cf7e6b11efb736afa436
Damngz <da.munozgz@gmail.com>
2024-10-27 12:49:18 -0300

    feat: controllers and session login

--- src/main/java/com/example/store_users/repository/UserRepository.java
+++ src/main/java/com/example/store_users/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.store_users.repository;
 
 import com.example.store_users.model.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository <User, Long> {
   Optional<User> findByEmail(String email);
+  Optional<User> findByEmailAndPassword(String email, String password);
 }
---
+++ src/main/java/com/example/store_users/service/UserService.java
@@ -0,0 +1,58 @@
+package com.example.store_users.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.store_users.model.User;
+import com.example.store_users.repository.UserRepository;
+
+@Service
+public class UserService {
+  @Autowired
+  private UserRepository userRepository;
+
+  public List<User> getAllUsers() {
+    return userRepository.findAll();
+  }
+
+  public Optional<User> getUserById(Long userId) {
+    return userRepository.findById(userId);
+  }
+
+  public Optional<User> getUserByEmail(String email) {
+    return userRepository.findByEmail(email);
+}
+
+  public User createUser(User user) {
+    return userRepository.save(user);
+  }
+
+  public User updateUser(Long userId, User updatedUser) {
+    return userRepository
+      .findById(userId)
+      .map(user -> {
+        user.setName(updatedUser.getName());
+        user.setEmail(updatedUser.getEmail());
+        user.setPassword(updatedUser.getPassword());
+        user.setRole(updatedUser.getRole());
+        return userRepository.save(user);
+      })
+      .orElseThrow(() -> new RuntimeException("User not found with id " + userId));
+  }
+
+  public void deleteUser(Long userId) {
+    if (userRepository.existsById(userId)) {
+      userRepository.deleteById(userId);
+    } else {
+      throw new RuntimeException("User not found with ID: " + userId);
+    }
+  }
+
+  public User authenticateUser(String email, String password) {
+    return userRepository.findByEmailAndPassword(email, password)
+      .orElseThrow(() -> new RuntimeException("Credenciales inválidas"));
+}
+}
