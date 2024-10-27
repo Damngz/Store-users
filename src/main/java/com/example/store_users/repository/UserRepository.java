42706e2970dc15715eaacf3c6737b3ac9895595b
Damngz <da.munozgz@gmail.com>
2024-10-27 01:48:36 -0300

    feat: repository

---
+++ src/main/java/com/example/store_users/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.store_users.repository;
+
+import com.example.store_users.model.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+  Optional<User> findByEmail(String email);
+}
