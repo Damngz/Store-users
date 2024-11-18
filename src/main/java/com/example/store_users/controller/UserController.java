af539303cb3162562d624f7f5334a402d2029a13
Damngz <da.munozgz@gmail.com>
2024-11-18 00:07:01 -0300

    fix: docker

--- src/main/java/com/example/store_users/controller/UserController.java
+++ src/main/java/com/example/store_users/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.example.store_users.service.UserService;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin("http://localhost")
 public class UserController {
   @Autowired
   private UserService userService;
